f229dbc2d5e1b647aac88c079b889a9557a4c347
xXLordDedXx <wiktorek2003gro@wp.pl>
2024-10-09 23:35:33 +0200

    Service Layer Mapping POST/PUT/DELETE

--- src/main/java/com/springboot/crude/rest/EmployeeRestController.java
+++ src/main/java/com/springboot/crude/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.springboot.crude.rest;
 import com.springboot.crude.entity.Employee;
 import com.springboot.crude.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,31 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    //Add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    //Add mapping for PUT /employees - update EXISTING employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    //Add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+        //Null Exception
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Employee deleted: " + employeeId;
+    }
 }
